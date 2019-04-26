9f730572acf62b64aa7baf4c370c1b47cf412b6f
Jørgen Vatle <Jorgen@WcAServices.net>
2019-04-26 13:23:27 +0300

    Add Webhook payload interface.

---
+++ src/Webhook.ts
@@ -0,0 +1,34 @@
+import Embed from './Embed';
+
+/**
+ * Discord webhook execution content.
+ *
+ * @link https://discordapp.com/developers/docs/resources/webhook#execute-webhook
+ */
+export default interface Webhook {
+    /**
+     * Message contents.
+     * (Max 2000 characters)
+     */
+    content: string;
+
+    /**
+     * Webhook username override
+     */
+    username?: string;
+
+    /**
+     * Whether or not this notification should be read as text to speech.
+     */
+    tts?: boolean;
+
+    /**
+     * Contents of a file being sent.
+     */
+    file?: any,
+
+    /**
+     * Embedded "rich" content.
+     */
+    embeds?: Embed[];
+}
