70b8c4b774cf8dd278bb974aef6f9f59f6810c6a
Jørgen Vatle <Jorgen@WcAServices.net>
2019-04-26 13:42:04 +0300

    Add EmbedField object interface.

---
+++ src/EmbedField.ts
@@ -0,0 +1,18 @@
+export default interface EmbedField {
+
+    /**
+     * Name of the field.
+     */
+    name: string;
+
+    /**
+     * Value of the field.
+     */
+    value: string;
+
+    /**
+     * Whether or not this field should be displayed inline.
+     */
+    inline: boolean;
+
+}
