693dd7a071ae2b365ec352927de5de6076bdc1bb
Jørgen Vatle <Jorgen@WcAServices.net>
2019-04-26 13:33:28 +0300

    Add EmbedProvider object interface.

---
+++ src/EmbedProvider.ts
@@ -0,0 +1,13 @@
+export default interface EmbedProvider {
+
+    /**
+     * Name of the provider.
+     */
+    name?: string;
+
+    /**
+     * URL of the provider.
+     */
+    url?: string;
+
+}
