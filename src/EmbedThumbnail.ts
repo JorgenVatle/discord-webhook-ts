d217c609f0bc1c708a9b4ad61ef4e84e52ce39a4
Jørgen Vatle <Jorgen@WcAServices.net>
2019-04-26 13:25:59 +0300

    Add EmbedThumbnail interface.

---
+++ src/EmbedThumbnail.ts
@@ -0,0 +1,22 @@
+export default interface EmbedThumbnail {
+    /**
+     * Source URL of the thumbnail.
+     * (Only supports http(s) and attachments)
+     */
+    url?: string;
+
+    /**
+     * A proxied URL of the thumbnail.
+     */
+    proxy_url?: string;
+
+    /**
+     * Height of the thumbnail.
+     */
+    height: number;
+
+    /**
+     * Width of the thumbnail.
+     */
+    width: number;
+}
