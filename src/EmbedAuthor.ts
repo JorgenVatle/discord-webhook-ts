26e11b0d16d114c5ebcae9381478d036c615a960
Jørgen Vatle <Jorgen@WcAServices.net>
2019-04-26 13:35:11 +0300

    Add EmbedAuthor object interface.

---
+++ src/EmbedAuthor.ts
@@ -0,0 +1,24 @@
+export default interface EmbedAuthor {
+
+    /**
+     * Name of the author.
+     */
+    name?: string;
+
+    /**
+     * URL of the author.
+     */
+    url?: string;
+
+    /**
+     * URL of the author icon.
+     * (Only supports HTTP(s) and attachments)
+     */
+    icon_url?: string;
+
+    /**
+     * Proxied URL of the author icon.
+     */
+    proxy_icon_url?: string;
+
+}
