668590db72d201180879ee24d17c3a016fd287ab
Jørgen Vatle <Jorgen@WcAServices.net>
2019-04-26 16:32:55 +0300

    Add tiny DiscordWebhook client.

---
+++ src/DiscordWebhook.ts
@@ -0,0 +1,32 @@
+import Webhook from '../types/Webhook';
+import Axios, { AxiosInstance } from 'axios';
+
+export default class DiscordWebhook {
+
+    /**
+     * Discord Webhook URL
+     */
+    private client: AxiosInstance;
+
+    /**
+     * Discord Webhook constructor
+     */
+    public constructor(webhookUrl: string) {
+        this.client = Axios.create({
+            url: webhookUrl,
+        });
+    }
+
+    /**
+     * Execute a webhook request.
+     *
+     * @param options
+     */
+    public execute(options: Webhook) {
+        return this.client.request({
+            method: 'POST',
+            data: options,
+        })
+    }
+
+}
