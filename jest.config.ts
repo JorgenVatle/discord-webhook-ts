e39048796e25b556dcfe8c104a90bf8479513a13
Jørgen Vatle <Jorgen@WcAServices.net>
2021-12-06 03:21:40 +0300

    Correct test directory path.

--- jest.config.ts
+++ jest.config.ts
@@ -118,7 +118,7 @@ export default {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    "/tests"
+    "tests"
   ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
